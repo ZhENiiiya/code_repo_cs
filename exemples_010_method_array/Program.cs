9194e1becf85190bfe802df716b8a6a0525aa008
Eugen Eugen <zhenya@localhost>
2022-12-21 21:03:07 +0300

    add exemples_010

---
+++ exemples_010_method_array/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {12, 5, 34, 22, 100, 27, 94,  45, 100};
+
+int n = array.Length;
+int find = 100;
+
+int index = 0;
+
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
